5039c278772f8095fa2974e2d65e1ad3695a2572
lijieyao@@ <47850366+wdmJieyao@users.noreply.github.com>
2021-04-15 19:53:27 +0800

    Update LeetCode_0002.java

--- LeetCode_0002.java
+++ LeetCode_0002.java
@@ -85,25 +85,27 @@ public class LeetCode_0002 {
         return rootNode;
     }
 
-    
+    /**
+     大佬优化方案
+    */
      public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(-1), pre = dummyHead;
-        int t = 0;
-        while (l1 != null || l2 != null || t != 0) {
-            if (l1 != null) {
-                t += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                t += l2.val;
-                l2 = l2.next;
-            }
-            pre.next = new ListNode(t % 10);
-            pre = pre.next;
-            t /= 10;
-        }
-
-        return dummyHead.next;
+       ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+            
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }   
+        return root.next;
     }
     
     static class ListNode {
